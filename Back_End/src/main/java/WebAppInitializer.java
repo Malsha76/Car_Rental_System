4f180cfdda02ca13d30121a5894569f2af6fc586
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2023-10-20 22:45:23 +0530

    webAppInitializer

---
+++ Back_End/src/main/java/WebAppInitializer.java
@@ -0,0 +1,28 @@
+import config.WebAppConfig;
+import config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
+/**
+ * @author Malsha Ekanayake
+ */
+
+public class WebAppInitializer extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+}
