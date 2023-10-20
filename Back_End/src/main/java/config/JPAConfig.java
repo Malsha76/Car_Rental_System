e5a5005baa5380f52a680911e9fa76418567d825
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2023-10-20 23:09:35 +0530

    JPA

--- Back_End/src/main/java/config/JPAConfig.java
+++ Back_End/src/main/java/config/JPAConfig.java
@@ -1,4 +1,25 @@
 package config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import javax.sql.DataSource;
+
+@Configuration
+//@EnableTransationManagement
 public class JPAConfig {
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory
+            (DataSource ){
+
+    }
+
+
+
 }
