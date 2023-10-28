5a7e4dc66e1851c397f401a0ea97e749d7fe2adc
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2023-10-28 19:02:51 +0530

    main js

--- Front_End/assets/js/main.js
+++ Front_End/assets/js/main.js
@@ -144,4 +144,18 @@ document.addEventListener('DOMContentLoaded',() => {
         }
     });
 
-})
+    //
+    function aos_init() {
+        AOS.init({
+            duration: 1000,
+            easing: 'ease-in-out',
+            once: true,
+            mirror: false
+        });
+    }
+    window.addEventListener('load', () => {
+        aos_init();
+    });
+
+});
+
