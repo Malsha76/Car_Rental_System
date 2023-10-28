a539943189c9ba457229139574e1ec3e26d110a6
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2023-10-28 18:32:52 +0530

    main js

--- Front_End/assets/js/main.js
+++ Front_End/assets/js/main.js
@@ -55,7 +55,7 @@ document.addEventListener('DOMContentLoaded',() => {
     const mobileNavHide = document.querySelector('.mobile-nav-hide');
 
     document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
-        el.addEventListener('click', function(event) {
+        el.addEventListener('click', function (event) {
             event.preventDefault();
             mobileNavToogle();
         })
@@ -66,4 +66,20 @@ document.addEventListener('DOMContentLoaded',() => {
         mobileNavShow.classList.toggle('d-none');
         mobileNavHide.classList.toggle('d-none');
     }
+
+    // Hide mobile nav on same-page/hash links
+    document.querySelectorAll('#navbar a').forEach(navbarlink => {
+
+        if (!navbarlink.hash) return;
+
+        let section = document.querySelector(navbarlink.hash);
+        if (!section) return;
+
+        navbarlink.addEventListener('click', () => {
+            if (document.querySelector('.mobile-nav-active')) {
+                mobileNavToogle();
+            }
+        });
+    });
+
 })
