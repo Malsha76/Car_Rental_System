5a7b8e3ded9e4cc50c9755a3d185161a74188fff
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2023-11-02 22:44:23 +0530

    backend

---
+++ Back_End/src/main/java/dto/PaymentDTO.java
@@ -0,0 +1,26 @@
+package dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Data
+public class PaymentDTO {
+
+    private String payment_ID;
+    private String rentID;
+    private String payment_type;
+    private LocalTime time;
+    private LocalDate date;
+    private Double lostDamage;
+    private Double rentFee;
+    private Double driverFee;
+    private Double total;
+}
