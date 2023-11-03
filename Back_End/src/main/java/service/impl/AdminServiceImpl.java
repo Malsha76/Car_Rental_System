78c7fcd3631df97c3de61bda7981e8d1ae39a23c
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2023-11-03 15:59:09 +0530

    backend

---
+++ Back_End/src/main/java/service/impl/AdminServiceImpl.java
@@ -0,0 +1,54 @@
+package service.impl;
+
+import dto.AdminDTO;
+import entity.Admin;
+import repo.AdminRepo;
+import service.AdminService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import repo.AdminRepo;
+
+import java.util.ArrayList;
+
+@Service
+@Transactional
+public class AdminServiceImpl implements AdminService {
+    @Autowired
+    private AdminRepo repo;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @Override
+    public void saveAdmin(AdminDTO dto) {
+        if (repo.existsById(dto.getUser_Id())) {
+            throw new RuntimeException("Admin Already Exist. Please enter another id..!");
+        }
+        repo.save(mapper.map(dto, Admin.class));
+    }
+
+    @Override
+    public void updateAdmin(AdminDTO dto) {
+        if (!repo.existsById(dto.getUser_Id())) {
+            throw new RuntimeException("Admin Not Exist. Please enter Valid id..!");
+        }
+        repo.save(mapper.map(dto, Admin.class));
+    }
+
+    @Override
+    public void deleteAdmin(String reg_ID) {
+        if (!repo.existsById(reg_ID)) {
+            throw new RuntimeException("Wrong ID..Please enter valid id..!");
+        }
+        repo.deleteById(reg_ID);
+    }
+
+    @Override
+    public ArrayList<AdminDTO> getAllAdmin() {
+        return mapper.map(repo.findAll(), new TypeToken<ArrayList<Admin>>() {
+        }.getType());
+    }
+}
