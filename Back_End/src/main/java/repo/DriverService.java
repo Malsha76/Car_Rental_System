3b648bd8df7aea2c98aa0ec07791878fd871d2c3
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2023-11-03 17:08:59 +0530

    backend

---
+++ Back_End/src/main/java/repo/DriverService.java
@@ -0,0 +1,23 @@
+package repo;
+
+import dto.CustomDTO;
+import dto.DriverDTO;
+import entity.Driver;
+
+import java.util.ArrayList;
+
+/**
+ * @author Malsha Ekanayaka
+ */
+public interface DriverService {
+    void saveDriver(DriverDTO dto);
+    void updateDriver(DriverDTO dto);
+    void deleteDriver(String reg_ID);
+    ArrayList<DriverDTO> getAllDriver();
+    ArrayList<DriverDTO> getAllAvalabileDriver();
+    CustomDTO userIdGenerate();
+    CustomDTO getSumAvailableDriver();
+    CustomDTO getSumUnavailableDriver();
+    Driver searchDriverId(String id);
+    CustomDTO getSumDriver();
+}
